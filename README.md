4b0855d0a6fb6986d03ef6691577c04fd7aef5e8
Dhanarajb <88299676+Dhanarajb@users.noreply.github.com>
2024-01-05 13:14:30 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# xactly-int
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/Dhanarajb/xactly-int)
---
+++ index.js
@@ -0,0 +1,73 @@
+// Ref
+// useRef vs react.ref
+// forwordRef
+// realExample for lifecycle method
+// which is the best lifecycle method foe asynchronus operation
+// how do you stop setinterval in lifecycle method in functional component
+// how do you center the data
+// What is promise? why we are using?
+// async await vs promise
+// useMemo vs useCallback with code
+// Redux
+// Redux-Toolkit
+// what is extraReducer
+// user imparative handle
+// let vs const vs var
+// how hoisting works internally?
+// Set vs MAp
+// Can we use loop in SCSS?
+// purpose of tabIndex
+// Call vs bind
+// This keyword
+// arrow function advantage
+// latest version of React
+// How do you integrate search component with Redux?
+// what pattern you used for Search Component?
+
+// let arr = [1, 2, 3, 1, 2, 5, 1, 1, 0];
+
+// const occurrenceArr = arr.reduce((acc,current)=>{
+//   acc[current]=(acc[current] || 0)+1
+//   return acc
+// },{})
+
+// console.log(occurrenceArr)
+// const uniquArr = [];
+// let n = arr.length;
+// let m = uniquArr.length;
+// for (let i = 0; i < n; i++) {
+//   if(uniquArr.indexOf(arr[i])===-1){
+//     uniquArr.push(arr[i])
+//   }
+// }
+// console.log(uniquArr);
+
+// let arr = [2, 3, 5];
+
+// Array.prototype.square = function () {
+//   for (let i = 0; i < this.length; i++) {
+//     this[i] = this[i] * this[i];
+//   }
+//   return this;
+// };
+
+// console.log(arr.square());
+
+// ur voice is breaking
+
+// setInterval(()=>{
+//   console.log("Hello")
+// },2000)
+
+// const myPromise = new Promise((resolve,reject)=>{
+//   setTimeout(()=>{
+//     const success =true;
+//     if(success){
+//       resolve("success")
+//     }else{
+//       reject("reject")
+//     }
+//   },2000)
+// })
+
+// myPromise.then((result)=>console.log(result)).catch((error)=>console.log(error))
